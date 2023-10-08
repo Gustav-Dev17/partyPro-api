ea553503c8cc9ff7be73f33ccc25f123be86712c
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2023-10-08 15:53:40 -0400

    🐛 Fix model bug

--- src/types/events.body.typs.ts
+++ src/types/events.body.typs.ts
@@ -2,6 +2,7 @@ export type EventTypes = "Online" | "FaceToFace";
 
 export interface IRequestEventBody {
   title?: string;
+  code: string;
   description?: string;
   location?: string;
   start_date?: string;
@@ -15,13 +16,14 @@ export interface IRequestEventBody {
 
 export interface IEvent {
   title: string;
+  code: string;
   description: string;
   location?: string;
   start_date: string;
   end_date?: string;
-  purchase_link?: string;
   type: EventTypes;
   url?: string;
+  purchase_link?: string;
   categories: string[];
   organiserId: string;
 }
